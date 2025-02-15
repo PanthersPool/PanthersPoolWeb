23de1a26f9df104f69edd99c321d5df5c8af555c
ZEYI TONG <zt2373@columbia.edu>
2025-02-15 16:49:47 -0500

    draft confirmation page and driver signup page

---
+++ panthers-pool/src/ConfirmationPage.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./Confirmation.css";
+
+function Confirmation() {
+    return (
+        <div className="confirmation-box">
+            
+        </div>
+    )
+}
+
+export default Confirmation
