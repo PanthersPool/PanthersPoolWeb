053bcc15b8ac17ceab3dd947842f3998883813ad
ZEYI TONG <zt2373@columbia.edu>
2025-02-15 16:51:41 -0500

    confirmation and driver signup page

--- panthers-pool/src/ConfirmationPage.jsx
+++ panthers-pool/src/ConfirmationPage.jsx
@@ -1,12 +1,34 @@
 import { useState } from "react";
-import "./Confirmation.css";
+import "./ConfirmationPage.css";
+import placeholderMap from "./assets/placeholder1.png";
 
-function Confirmation() {
+function ConfirmationPage() {
     return (
-        <div className="confirmation-box">
-            
+        <div className="confirmation-page">
+            <div className="confirmation-container">
+                <div className="confirmation-card">
+                    <h2>Confirmation</h2>
+                    <p className="price">$<strong>30</strong></p>
+                    <ul>
+                        <li>ADK to Burlington Airport</li>
+                        <li>March 9</li>
+                        <li>11:00 AM</li>
+                        <li>1 Passenger</li>
+                        <li>1 Large Suitcase</li>
+                    </ul>
+                    <button className="confirm-button">Confirm</button>
+                </div>
+
+                <div className="footer">
+                    <p><strong>Ride Together, Thrive Together.</strong></p>
+                    <a href="/" className="portal-link">Portal page →</a>
+                </div>
+            </div>
+            <div className="google-map">
+                <img className="place-holder-map" src={placeholderMap} alt="Google Map Placeholder"/>
+            </div>
         </div>
     )
 }
 
-export default Confirmation
+export default ConfirmationPage
