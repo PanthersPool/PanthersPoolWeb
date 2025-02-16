41df44c13e386aee3b3572b529fd1f6eb0f3680b
Josef <liemjt@comcast.net>
2025-02-16 16:25:40 -0500

    Initial draft of Fine Ride Page

--- panthers-pool/src/App.jsx
+++ panthers-pool/src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LaunchPage from "./LaunchPage";
+import FindRide from "./FindRide";
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <LaunchPage /> {/* Render LaunchPage component */}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LaunchPage />} />
+        <Route path="/find-ride" element={<FindRide />} />
+      </Routes>
+    </Router>
   );
 }
 
--- panthers-pool/src/LaunchPage.jsx
+++ panthers-pool/src/LaunchPage.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import NavBar from "./NavBar";
 import "./LaunchPage.css";
 import "./NavBar.css";
 
 const LaunchPage = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="launch-page">
-      <NavBar /> {}
+      <NavBar />
       <div className="center-buttons">
-        <button className="need-ride">I need a ride </button>
+        <button className="need-ride" onClick={() => navigate("/find-ride")}>
+          I need a ride
+        </button>
         <button className="will-drive">I am driving</button>
       </div>
     </div>
